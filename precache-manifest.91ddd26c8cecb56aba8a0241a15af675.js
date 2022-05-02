7fd83749d44d517f76b80b480808600fa15d9063
Code Sandbox <hello@codesandbox.io>
2022-05-02 14:19:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.91ddd26c8cecb56aba8a0241a15af675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71fd21c49ec066a492e6",
+    "url": "https://dhana-krishnasamy.github.io/csb-ux4t27/static/css/main.4b28b434.chunk.css"
+  },
+  {
+    "revision": "71fd21c49ec066a492e6",
+    "url": "https://dhana-krishnasamy.github.io/csb-ux4t27/static/js/main.71fd21c4.chunk.js"
+  },
+  {
+    "revision": "0f53f0b08e233a73ddc0",
+    "url": "https://dhana-krishnasamy.github.io/csb-ux4t27/static/js/1.0f53f0b0.chunk.js"
+  },
+  {
+    "revision": "a10e7e0dac2986122d7d",
+    "url": "https://dhana-krishnasamy.github.io/csb-ux4t27/static/js/runtime~main.a10e7e0d.js"
+  },
+  {
+    "revision": "41e06b955508d4f73bea2d5d91332eb0",
+    "url": "https://dhana-krishnasamy.github.io/csb-ux4t27/static/media/stylis.41e06b95.mjs"
+  },
+  {
+    "revision": "c9144a6d198eeb59eabddae21739f56e",
+    "url": "https://dhana-krishnasamy.github.io/csb-ux4t27/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://dhana-krishnasamy.github.io/csb-ux4t27/precache-manifest.91ddd26c8cecb56aba8a0241a15af675.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://dhana-krishnasamy.github.io/csb-ux4t27/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
